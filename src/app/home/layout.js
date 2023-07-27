861655942163ff7ab6a1c92657a1c42ae7502fa7
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-07-27 16:56:44 -0500

    refactor homepage

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,32 +1,28 @@
+import Image from "next/image"
 import Link from "next/link"
 export default function HomeLayout({
     children
 }) {
-    const nav_bar = "p-4 border border-black hover:border-white"
+    const nav_bar = "p-4 border border-transparent hover:border-white"
     return(
-        <main className="flex flex-col min-h-screen bg-white">
-            <div className="flex flex-row text-black bg-gray-700">
-                <div className={nav_bar}>
-                <Link href="/home">
-                    Home 
-                </Link>
-                </div>
-                <div className={nav_bar}>
-                <Link href="/home/teams">
-                    Teams 
-                </Link>
-                </div>
-                <div className={nav_bar}>
-                <Link href="/home/players">
-                    Players 
-                </Link>
-                </div>
-                <div className={nav_bar}>
-                <Link href="/home/leaderboard">
-                    Leaderboard    
-                </Link>
-                </div>
-            </div>
+        <main className="flex flex-col min-h-screen bg-amber-800">
+            <menu className="flex flex-row bg-orange-900">
+                <li className={nav_bar}>
+                    <Link href="/home">Home</Link>
+                </li>
+                <li className={nav_bar}>
+                    <Link href="/home/teams">Teams</Link>
+                </li>
+                <li className={nav_bar}>
+                    <Link href="/home/players">Players</Link>
+                </li>
+                <li className={nav_bar}>
+                    <Link href="/home/leaderboard">Leaderboard</Link>
+                </li>
+                <li className="absolute right-4">
+                    <Image src="/csgo.png" alt="CSGO Logo" width={60} height={60}/>
+                </li>
+            </menu>
             {children}
         </main>
 )}
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,6 @@
-import Image from "next/image"
 export default function Home() {
     return (
         <main className="flex flex-col">
-            <Image src="/csgo.png" alt="CSGO Logo" width={150} height={150} className="self-center"/>
-            <h2 className="text-black text-center">All Your Favorite CSGO Stats in One Place</h2>
+            <h2 className="text-black text-center text-4xl relative top-8">All Your Favorite CSGO Stats in One Place</h2>
         </main>
 )}
