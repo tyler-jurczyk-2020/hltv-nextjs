a6c89de7ffed38703f83923ca5d7be857d7d54f1
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-07-27 00:14:32 -0500

    Added IBM font and fleshed out nav bar

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -2,22 +2,30 @@ import Link from "next/link"
 export default function HomeLayout({
     children
 }) {
-    const nav_bar = "p-4 hover:bg-blue-400"
+    const nav_bar = "p-4 border border-black hover:border-white"
     return(
-        <main className="flex flex-col min-h-screen bg-gradient-to-b from-gray-600 to-black-300">
-            <div className="flex flex-row border border-blue-600 text-black bg-orange-400">
-                <Link href="/home" className={nav_bar}>
+        <main className="flex flex-col min-h-screen bg-white">
+            <div className="flex flex-row font-ttf text-black bg-gray-700">
+                <div className={nav_bar}>
+                <Link href="/home">
                     Home 
                 </Link>
-                <Link href="/home/teams" className={nav_bar}>
+                </div>
+                <div className={nav_bar}>
+                <Link href="/home/teams">
                     Teams 
                 </Link>
-                <button className={nav_bar}>
+                </div>
+                <div className={nav_bar}>
+                <Link href="/home/players">
                     Players 
-                </button>
-                <button className={nav_bar}>
+                </Link>
+                </div>
+                <div className={nav_bar}>
+                <Link href="/home/leaderboard">
                     Leaderboard    
-                </button>
+                </Link>
+                </div>
             </div>
             {children}
         </main>
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,8 @@
 import Image from "next/image"
 export default function Home() {
     return (
-        <main className="flex flex-col">
+        <main className="flex flex-col font-ttf font-bold">
             <Image src="/csgo.png" alt="CSGO Logo" width={150} height={150} className="self-center"/>
-            <h2 className="text-black text-center">All Your Favorite CSGO Stats in One Place</h2>)
+            <h2 className="text-black text-center">All Your Favorite CSGO Stats in One Place</h2>
         </main>
 )}
