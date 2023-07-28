0902fcc334b8004bcc17fb45bd7c010e6676adf0
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-07-27 19:05:33 -0500

    should be with last commit

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -7,11 +7,11 @@ export default function HomeLayout({
     return(
         <main className="flex flex-col min-h-screen bg-amber-800">
             <menu className="flex flex-row bg-orange-900">
-                    <Link className={nav_bar} href="/home">Home</Link>
-                    <Link className={nav_bar} href="/home/teams">Teams</Link>
-                    <Link className={nav_bar} href="/home/players">Players</Link>
-                    <Link className={nav_bar} href="/home/leaderboard">Leaderboard</Link>
-                    <Image src="/csgo.png" alt="CSGO Logo" width={60} height={60} className="ml-auto mr-4"/>
+                <Link className={nav_bar} href="/home">Home</Link>
+                <Link className={nav_bar} href="/home/teams">Teams</Link>
+                <Link className={nav_bar} href="/home/players">Players</Link>
+                <Link className={nav_bar} href="/home/leaderboard">Leaderboard</Link>
+                <Image src="/csgo.png" alt="CSGO Logo" width={60} height={60} className="ml-auto mr-4"/>
             </menu>
             {children}
         </main>
