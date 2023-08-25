e5b4c1fa9563705efd2c508f67a4fea391e4298e
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-08-24 22:49:44 -0500

    First iteration of projects page. Now using counter for React keys

--- src/app/home/Popout.js
+++ src/app/home/Popout.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
 
 export default function({main, className, text, elements}) {
+    let counter = -1;
     return(
     <div className="grid group">
         <Link href={main} className={className}>{text}</Link>
         <div className="justify-self-center translate-y-[58px] w-52 h-0 absolute bg-neutral-900 group-hover:h-52 duration-700 overflow-hidden flex flex-col">
-            {elements.map((val, idx) => (<Link key={idx} href={val[1]} className="text-center border border-neutral-800 p-4">{val[0]}</Link>))}
+            {elements.map((val) => (<Link key={counter--} href={val[1]} className="text-center border border-neutral-800 p-4">{val[0]}</Link>))}
         </div>
     </div>
 )}
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -15,6 +15,7 @@ export default async function Home() {
     // Get JSON Data
     const resumeReq = await fetch("http://localhost:3000/home/api", {cache: 'no-store'});
     const resume = await resumeReq.json(); 
+    let counter = 0;
     return (
     <main>
         <menu className="z-50 fixed flex flex-row justify-center items-center bg-neutral-900 border-b-2 border-black w-screen h-nav-b]">
@@ -45,12 +46,17 @@ export default async function Home() {
                     <p className='col-span-3 text-2xl text-center self-center'>"I am a Computer Engineer from the University of Illinois Urbana-Champaign, and I am in the business of building products oriented towards the future. This website that are you are currently looking at was built using the frameworks/language highlighted above. Scroll furthur to see to learn a little more about me."</p>
                 </div>
                 <div className="grid min-h-screen content-start -mt-nav-b pt-nav-b" id="portfolio">
-        {/*<RotatingTxt className="z-0 text-5xl border w-min" text="Courses" content={["test1","test2", "test3"]} contentStyle="p-2 sticky top-[100px]" />*/}
                     <SlidingTxt className="text-5xl text-center p-12 pb-8" text="Recent Courses" direction="translate-x-[100vw]" duration="duration-1000"/>
-                    {resume.courses.map((item, index) => (<SlidingTxt key={index} className="text-2xl p-8 text-center" text={item} direction="-translate-x-[100vw]" duration="duration-1000"/>))} 
+                    {resume.courses.map((item) => (<SlidingTxt key={counter++} className="text-2xl p-8 text-center" text={item} direction="-translate-x-[100vw]" duration="duration-1000"/>))} 
                 </div>
                 <div id='projects' className="min-h-screen">
-                {/* Placeholder for projects*/}     
+                    <SlidingTxt className="text-5xl text-center" text="Projects" direction="translate-x-[100vw]" duration="duration-1000"/>
+                    {resume.projects.map((item) => (
+                    <div key={counter++}>
+                        <SlidingTxt key={counter++} className="text-2xl p-8 text-center" text={item.name} direction="-translate-x-[100vw]" duration="duration-1000"/>
+                        <SlidingTxt key={counter++} className="text-2xl p-8 text-center" text={item.description} direction="-translate-x-[100vw]" duration="duration-1000"/>
+                    </div>
+                    ))}
                 </div>
                 <div className="relative h-[calc(100vh-58px)]">
                     <FadeTxt className="text-5xl text-center p-12" text="Contact Info & Social" duration="duration-[3s]"/>
